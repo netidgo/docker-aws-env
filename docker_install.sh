beb0303d90fce6c4721648b11ff3cd1587fcdc0d
SOLDESK-김금목 <netidgo@gmail.com>
2026-07-09 16:41:00 +0900

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt-get update -y
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker ubuntu
+newgrp docker
+
+echo "alias doc=docker" >> /home/ubuntu/.bashrc
+alias doc=docker
